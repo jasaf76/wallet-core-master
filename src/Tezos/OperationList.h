867a7de38159d5263726428fab7c0b5f4d75ba88
jasaf76 <betreutes.wohnen.substituierte@gmail.com>
2022-08-30 22:46:54 +0200

    inicio

---
+++ src/Tezos/OperationList.h
@@ -0,0 +1,30 @@
+// Copyright © 2017-2022 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#pragma once
+
+#include "../Data.h"
+#include "proto/Tezos.pb.h"
+#include "../PrivateKey.h"
+#include <string>
+
+using namespace TW::Tezos;
+using namespace TW::Tezos::Proto;
+
+namespace TW::Tezos {
+
+class OperationList {
+  public:
+    std::string branch;
+    std::vector<Operation> operation_list;
+    OperationList(const std::string& string);
+    void addOperation(const Operation& transaction);
+    /// Returns a data representation of the operations.
+    Data forge(const PrivateKey& privateKey) const;
+    Data forgeBranch() const;
+};
+
+} // namespace TW::Tezos
